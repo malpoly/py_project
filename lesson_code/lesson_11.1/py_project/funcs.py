c004d767e1c9a065619286a34f06d28973b18268
mary <marry2691@yandex.ru>
2023-02-15 21:46:06 +0300

    funcs.py add new def

--- lesson_code/lesson_11.1/py_project/funcs.py
+++ lesson_code/lesson_11.1/py_project/funcs.py
@@ -2,4 +2,6 @@ def sum_two(num_a, num_b):
     '''Складывает 2 числа'''
     return num_a + num_b
 
-
+def subtraction_two(num_a, num_b):
+    '''Производит вычитание двух чисел'''
+    return num_a - num_b
